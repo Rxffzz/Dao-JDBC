e06eaa92ab43f3789c85062d1740be532c43476e
Rxffzz <srafael0406@gmail.com>
2024-09-03 07:34:44 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,8 +2,10 @@ package application;
 
 import db.DB;
 import model.Department;
+import model.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +13,10 @@ public class Main {
         Department obj = new Department(1, "Books");
 
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 2000.0, obj);
+
+        System.out.println(seller);
 
     }
 }
