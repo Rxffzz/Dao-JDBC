e2e4461f4a7102d4a7208761576dd7008a6906af
Rxffzz <srafael0406@gmail.com>
2024-09-04 07:37:35 -0300

    findById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,16 @@
 package application;
 
-import db.DB;
-import model.Department;
 import model.Seller;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 
-import java.sql.Connection;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 2000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
--- src/model/Seller.java
+++ src/model/Seller.java
@@ -100,4 +100,5 @@ public class Seller implements Serializable {
                 ", department=" + department +
                 '}';
     }
+
 }
